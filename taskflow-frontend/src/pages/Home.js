6ff0746bee6c49d4a68aec0ccc12db94f817fe6b
Krabel Lian <lk230@hdm-stuttgart.de>
2025-06-05 21:55:15 +0200

    neues design + buttons "all" + "completed" ohne logik

--- taskflow-frontend/src/pages/Home.js
+++ taskflow-frontend/src/pages/Home.js
@@ -6,32 +6,34 @@ import editIcon from "../assets/edit_icon.svg";
 function Home() {
   const [taskInput, setTaskInput] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('all');
 
   const handleDeleteTask = (indexToDelete) => {
     setTasks(tasks.filter((_, index) => index !== indexToDelete));
-  }
+  };
 
   const handleAddTask = () => {
     if (taskInput.trim() === '') return;
-    setTasks([...tasks, { text: taskInput, isEditing: false}]);
+    setTasks([...tasks, { text: taskInput, isEditing: false }]);
     setTaskInput('');
   };
 
-  const handleEditTask= (index) => {
+  const handleEditTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks[index].isEditing = true;
     setTasks(updatedTasks);
-  }
+  };
 
-  const handleSaveTask = (index, newText) =>{
+  const handleSaveTask = (index, newText) => {
     const updatedTasks = [...tasks];
     updatedTasks[index].text = newText;
     updatedTasks[index].isEditing = false;
     setTasks(updatedTasks);
-  }
+  };
+
   return (
     <div className="home">
-      <h1>taskflow</h1>
+      <h1>taskflow.</h1>
 
       <div className="input-box">
         <input
@@ -55,37 +57,51 @@ function Home() {
             <input type="checkbox" className="task-checkbox" />
 
             {task.isEditing ? (
-              <input  
-              className="task-input"
-              autoFocus
-              value={task.text}
-              onChange={(e) => {
-                const updatedTasks = [...tasks];
-                updatedTasks[index].text = e.target.value;
-                setTasks(updatedTasks);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter'){
-                  handleSaveTask(index, task.text);
-                }
-              }}
-              onBlur={() => handleSaveTask(index, task.text)}
+              <input
+                className="task-input"
+                autoFocus
+                value={task.text}
+                onChange={(e) => {
+                  const updatedTasks = [...tasks];
+                  updatedTasks[index].text = e.target.value;
+                  setTasks(updatedTasks);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    handleSaveTask(index, task.text);
+                  }
+                }}
+                onBlur={() => handleSaveTask(index, task.text)}
               />
             ) : (
               <span>{task.text}</span>
             )}
-            
 
             <div className="task-actions">
-            <button className="edit-button" onClick={() => handleEditTask(index)}>
-              <img src={editIcon} alt="Edit Task" className="icon"/>
-            </button>
-            <button className="delete-button" onClick={() => handleDeleteTask(index)}>
+              <button className="edit-button" onClick={() => handleEditTask(index)}>
+                <img src={editIcon} alt="Edit Task" className="icon" />
+              </button>
+              <button className="delete-button" onClick={() => handleDeleteTask(index)}>
                 <img src={deleteIcon} alt="Delete Task" className="icon" />
-            </button>
-          </div>
+              </button>
+            </div>
           </div>
         ))}
+      </div>
+
+      <div className="filter-buttons">
+        <button
+          onClick={() => setActiveFilter('all')}
+          className={`filter-button ${activeFilter === 'all' ? 'active' : ''}`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setActiveFilter('completed')}
+          className={`filter-button ${activeFilter === 'completed' ? 'active' : ''}`}
+        >
+          Completed
+        </button>
       </div>
     </div>
   );
